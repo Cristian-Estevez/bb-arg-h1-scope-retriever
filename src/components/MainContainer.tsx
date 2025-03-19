e4eacd67109a1b21dedae15c3117417ce0489a55
Cristian Estevez <cuentacursosprogramacion@gmail.com>
2025-03-19 20:58:23 -0300

    ui fixes

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -4,7 +4,7 @@ export default function MainContainer({ children }: PropsWithChildren) {
   return (
     <main className='w-full min-w-[400px] h-full'>
       <div className='flex flex-col items-center w-full h-full'>
-        <div className='flex flex-col gap-4 items-center w-full justify-center p-2'>
+        <div className='flex flex-col gap-4 items-center w-full justify-center'>
           {children}
         </div>
       </div>
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -10,7 +10,7 @@ export default function PageHeader({ children }: PropsWithChildren) {
       url: browser.runtime.getURL('index.html'),
       type: 'popup',
       width: 600,
-      height: 400
+      height: 432
     })
   }
 
